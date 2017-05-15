e978abf0bc0f89d372098bb3694be8ac48e662f2
dibomu <MukhopadhyayD0116@sduhsd.lan>
2017-05-15 09:27:20 -0700

    Add Age, Add Address, Dibyabrata
    Completed the Add age, Add address, does not work without error. Data
    not added correctly.

--- MyContactApp2/app/src/main/java/com/example/mukhopadhyayd0116/mycontactapp/DatabaseHelper.java
+++ MyContactApp2/app/src/main/java/com/example/mukhopadhyayd0116/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.mukhopadhyayd0116.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,6 +13,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME="contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2="NAME";
+    public static final String COL_3="AGE";
+    public static final String COL_4="ADDRESS";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
@@ -26,5 +30,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
         onCreate(db);
+    }
+    public boolean insertData(String name,String age, String address){
+        SQLiteDatabase db= this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,age);
+        contentValues.put(COL_4,address);
+
+        long result= db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1)return false;
+        else return true;
     }
 }
--- MyContactApp2/app/src/main/java/com/example/mukhopadhyayd0116/mycontactapp/MainActivity.java
+++ MyContactApp2/app/src/main/java/com/example/mukhopadhyayd0116/mycontactapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mukhopadhyayd0116.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
+
 
 
 
@@ -19,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         myDb=  new DatabaseHelper(this);
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editAddress = (EditText) findViewById(R.id.editText_Address);
     }
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(),editAddress.getText().toString());
+
+        if(isInserted==true){
+            //create toast message to user indicating data inserted correctly
+            Log.d("MyContact","Data Insertion successful");
+        }
+        else{
+            //create toast message to user indicating data inserted correctly
+            Log.d("MyContact","Data Insertion successful");
+
+        }
+    }
+
+
+
 }
